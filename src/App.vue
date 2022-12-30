4da4e07f4d102ef2de88a6f1bb0979593063e040
LucaArcudi <arcudilu@gmail.com>
2022-12-30 23:27:09 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import AppTitleVue from './components/AppTitle.vue';
+
+export default {
+  components: {
+    AppTitleVue
+  }
+}
+</script>
+
+<template>
+  <AppTitleVue />
+</template>
+
+<style scoped>
+
+</style>
